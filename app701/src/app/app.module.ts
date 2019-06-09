41810cdf581b94f57fbc61b7ea5d574097244c02
victor <shi5588@gmail.com>
2019-06-09 21:08:12 +0800

    11

--- app701/src/app/app.module.ts
+++ app701/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+//import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
